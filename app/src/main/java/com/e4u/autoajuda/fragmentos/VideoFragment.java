60b6e7467a99d7b9ecfe01090d4e404d333cff00
alanalvessantos <alanalvessantos2010@gmail.com>
2020-09-06 10:28:39 -0300

    commentaniroo

--- app/src/main/java/com/e4u/autoajuda/fragmentos/VideoFragment.java
+++ app/src/main/java/com/e4u/autoajuda/fragmentos/VideoFragment.java
@@ -7,22 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.e4u.autoajuda.R;
+import com.e4u.autoajuda.modelos.VideoModelo;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VideoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class VideoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    VideoModelo videoModelo = new VideoModelo();
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +29,6 @@ public class VideoFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VideoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static VideoFragment newInstance(String param1, String param2) {
         VideoFragment fragment = new VideoFragment();
         Bundle args = new Bundle();
@@ -55,12 +45,33 @@ public class VideoFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        criarListaDeVideos();
+
+        WebView displayYoutubeVideo = (WebView) getView().findViewById(R.id.mWebView);
+        displayYoutubeVideo.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        WebSettings webSettings = displayYoutubeVideo.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        displayYoutubeVideo.loadData(videoModelo.getVideoURL(), "text/html", "utf-8");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_video, container, false);
+    }
+
+    private void criarListaDeVideos(){
+        videoModelo.setVideoID(1);
+        videoModelo.setVideoImagem("");
+        videoModelo.setVideoTitle("MOTIVAÇÃO FORTE PARA TODOS OS DIAS DA SEMANA - MOTIVACIONAL NANDO PINHEIRO");
+        videoModelo.setVideoURL("https://www.youtube.com/embed/ETzMjtUpKYs");
+
     }
 }
---
+++ app/src/main/java/com/e4u/autoajuda/modelos/VideoModelo.java
@@ -0,0 +1,41 @@
+package com.e4u.autoajuda.modelos;
+
+public class VideoModelo {
+
+    private int videoID;
+    private String videoTitle;
+    private String videoURL;
+    private String videoImagem;
+
+    public int getVideoID() {
+        return videoID;
+    }
+
+    public void setVideoID(int videoID) {
+        this.videoID = videoID;
+    }
+
+    public String getVideoTitle() {
+        return videoTitle;
+    }
+
+    public void setVideoTitle(String videoTitle) {
+        this.videoTitle = videoTitle;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getVideoImagem() {
+        return videoImagem;
+    }
+
+    public void setVideoImagem(String videoImagem) {
+        this.videoImagem = videoImagem;
+    }
+}
