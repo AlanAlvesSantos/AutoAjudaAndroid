8a7193bac9eb0190c32d89cd65b7bc760a553da2
Felipe Carvalho <felipe.carvalho@faurecia-aptoide.com>
2020-11-07 13:59:42 +0000

    Finalize diario repo class

--- app/src/main/java/com/e4u/autoajuda/database/DiarioRepositorio.java
+++ app/src/main/java/com/e4u/autoajuda/database/DiarioRepositorio.java
@@ -3,74 +3,74 @@ package com.e4u.autoajuda.database;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Room;
 
 import com.e4u.autoajuda.modelos.DiarioModel;
+
+import java.util.List;
 
 public class DiarioRepositorio {
 
     private String DB_NAME = "db_auto_ajuda";
 
-    private DatabaseAutoAjuda noteDatabase;
-    public DiarioRepositorio (Context context) {
-        noteDatabase = Room.databaseBuilder (context, DatabaseAutoAjuda.class, DB_NAME) .build ();
+    private DatabaseAutoAjuda bancodeDadosAutoAjuda;
+
+    public DiarioRepositorio(Context context) {
+        bancodeDadosAutoAjuda = Room.databaseBuilder(context, DatabaseAutoAjuda.class, DB_NAME).build();
     }
 
-    public void inserirDiario (DiarioModel diario) {
+    //Salvar diario
+    public void inserirDiario(DiarioModel diario) {
 
-        salvarDiario (diario);
+        salvarDiario(diario);
     }
 
-    private void salvarDiario (DiarioModel diario) {
+    private void salvarDiario(DiarioModel diario) {
         new AsyncTask<Void, Void, Void>() {
             @Override
-            protected Void doInBackground (Void ...
-                                                   voids ) { noteDatabase.daoAccess (). insertTask (note);
+            protected Void doInBackground(Void...
+                                                  voids) {
+
+                bancodeDadosAutoAjuda.diarioDAO().inserirDiario(diario);
                 return null;
             }
-        } .execute ();
+        }.execute();
     }
 
-    public void updateTask (nota final) {
-        note.setModifiedAt (AppUtils.getCurrentDateTime ());
 
-        new AsyncTask <Void, Void, Void> () {
+    //Alterar dados do diario
+    public void alterarDiario(DiarioModel diarioModel) {
+
+        new AsyncTask<Void, Void, Void>() {
             @Override
-            protected Void doInBackground (Void ...
-                                                   voids ) { noteDatabase.daoAccess (). updateTask (nota);
+            protected Void doInBackground(Void...
+                                                  voids) {
+
+                bancodeDadosAutoAjuda.diarioDAO().alterarDiario(diarioModel);
                 return null;
             }
-        } .execute ();
+        }.execute();
     }
 
-    public void deleteTask (final int id) {
-        final LiveData <Note> task = getTask (id);
-        if (task! = null) {
-            new AsyncTask <Void, Void, Void> () {
-                @Override
-                protected Void doInBackground (Void ... voids) {
-                    noteDatabase.daoAccess (). deleteTask (task.getValue ());
-                    return null;
-                }
-            } .execute ();
-        }
-    }
+    //Excluir diario
 
-    public void deleteTask (nota final) {
-        new AsyncTask <Void, Void, Void> () {
+    public void excluirDiario(DiarioModel diarioModel) {
+
+        new AsyncTask<Void, Void, Void>() {
             @Override
-            protected Void doInBackground (Void ...
-                                                   voids ) { noteDatabase.daoAccess (). deleteTask (note);
+            protected Void doInBackground(Void...
+                                                  voids) {
+
+                bancodeDadosAutoAjuda.diarioDAO().excluirDiario(diarioModel);
                 return null;
             }
-        } .execute ();
+        }.execute();
     }
 
-    public LiveData <Nota> getTask (int id) {
-        return noteDatabase.daoAccess (). getTask (id);
-    }
+    //Retornar Lista dos dias do diario
+    public LiveData<List<DiarioModel>> retornarListaDiario() {
 
-    public LiveData <List <Note>> getTasks () {
-        return noteDatabase.daoAccess (). fetchAllTasks ();
+        return bancodeDadosAutoAjuda.diarioDAO().retornarDiario();
     }
 }
